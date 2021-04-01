457aeab1dfb01f1996fdb63b09f93489e0ce8d4e
LAPTOP-MQHP0OUO\ASUS <vangol2013@gmail.com>
2021-04-01 09:33:51 +0700

    commit to switch to personal branch

--- src/component/ListRestaurant/apiService.js
+++ src/component/ListRestaurant/apiService.js
@@ -2,8 +2,24 @@ import axiosClient from "../../api";
 import URL from "../../api/url";
 
 const APIService = {
-  getListRestaurant: async () => {
-    return await axiosClient.get(URL.RESTAURANT.GET_LIST);
+  getListRestaurant: async (page) => {
+    page = page || 1;
+    return await axiosClient.get(URL.RESTAURANT.GET_LIST_ALL);
+  },
+
+  getListRestaurantNew: async (page) => {
+    page = page || 1;
+    return await axiosClient.get(URL.RESTAURANT.GET_LIST_NEW+ `?page=${page}`);
+  },
+
+  getListRestaurantBestDiscount: async (page) => {
+    page = page || 1;
+    return await axiosClient.get(URL.RESTAURANT.GET_LIST_DISCOUNT+ `?page=${page}`);
+  },
+
+  getListRestaurantNearest: async (page) => {
+    page = page || 1;
+    return await axiosClient.get(URL.RESTAURANT.GET_LIST_NEAREST+ `?page=${page}`);
   },
 };
 
--- src/component/ListRestaurant/index.js
+++ src/component/ListRestaurant/index.js
@@ -21,34 +21,57 @@ const Footer = () => {
   const history = useHistory();
   // use dispatch
   const dispatch = useDispatch();
+  // use state
+  const [pageIndex, setPageIndex] = useState(1);
 
   const data = [{"Location":{"type":"Point","coordinates":[106.799117,10.875547]},"Type":0,"Status":0,"ParkingFree":0,"_id":"6040cbdac0c37e0cc405a85e","Name":"Lẩu bò Việt Nam","ContractID":"20210101T2","OpenAt":"1899-12-31T00:47:56.000Z","CloseAt":"1899-12-31T14:37:56.000Z","Description":"Quán lẩu bò lớn nhất Đông Nam Á","Avatar":"https://source.unsplash.com/random/800x600","Anouncement":"Miễn phí tất cả các suất ăn vào ngày 31/2/2021","Address":"Làng đại học quốc gia TPHCM","CreatedAt":"2021-03-04T12:00:26.179Z","UpdatedAt":"2021-03-04T12:00:26.179Z","__v":0},{"Location":{"type":"Point","coordinates":[106.78785864259649,10.867934698321426]},"Type":0,"Status":0,"ParkingFree":0,"_id":"6040cc5673b0831cc8014a49","Name":"Hải sản Việt Nam","ContractID":"20210101T5","OpenAt":"1899-12-31T08:47:56.000Z","CloseAt":"1899-12-31T15:17:56.000Z","Description":"Quán hải sản lớn nhất Thế giới","Avatar":"https://source.unsplash.com/random/800x600","Anouncement":"Miễn phí tất cả các suất ăn vào ngày 31/2/2021","Address":"Chợ đêm ĐH Nông Lâm TPHCM","CreatedAt":"2021-03-04T12:02:30.227Z","UpdatedAt":"2021-03-04T12:02:30.227Z","__v":0}];
   
   const listRestaurant = DataUtils.mapDataListRestaurant(data);
-  // useEffect(() => {
-  //   //turnOnLoading();
 
-  //   (async () => {
-  //     try {
-  //       const {
-  //         success,
-  //         message,
-  //         data,
-  //       } = await apiService.getListRestaurant();
+  const onChangePage = (index) => {
+      if (pageIndex !== index) {
+        setPageIndex(index);
+      } 
+  }
 
-  //       console.log("dataaa: " + success)
-  //       if (success) {
-  //           console.log("data: " + JSON.stringify(data))
-  //       }
-  //     } catch (e) {
-  //       // alert("Cannot connect to server");
-  //       // console.log("[ERROR]:", e);
-  //     }
+  const onPreviousPage = () => {
+    const index = pageIndex - 1;
+    if (index > 0) {
+      setPageIndex(index);
+    }
+  };
 
-  //     // turnOffLoading();
-  //   })();
-  //   // eslint-disable-next-line react-hooks/exhaustive-deps
-  // }, []);
+  const countPage = 7
+  const onNextPage = () => {
+    const index = pageIndex + 1;
+    if (index <= countPage) {
+      setPageIndex(index);
+    }
+  };
+
+  useEffect(() => {
+    //turnOnLoading();
+
+    (async () => {
+      try {
+        const {
+          success,
+          message,
+          data,
+        } = await apiService.getListRestaurant();
+
+        if (success) {
+            console.log("data: " + JSON.stringify(data));
+        }
+      } catch (e) {
+        // alert("Cannot connect to server");
+        // console.log("[ERROR]:", e);
+      }
+
+      // turnOffLoading();
+    })();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -75,9 +98,13 @@ const Footer = () => {
               <Grid item md={12}>
                 <div className="pagination">
                   <Pagination
+                    activePage={pageIndex}
                     itemPerPage={10}
                     pageRangeDisplay={10}
                     totalItemsCount={67}
+                    onChangePage={onChangePage}
+                    onPreviousPage={onPreviousPage}
+                    onNextPage={onNextPage}
                   />
                 </div>
               </Grid>
