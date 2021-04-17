07606b7472de7c491b9d1f3bbad309f463764b82
PLHB1712289 <phanlehoaibaok10@gmail.com>
2021-04-17 16:11:06 +0700

    add map

--- src/component/HomePage/DialogChangeAddress/index.js
+++ src/component/HomePage/DialogChangeAddress/index.js
@@ -1,20 +1,17 @@
-import React, { useState } from "react";
-import Button from "@material-ui/core/Button";
-import IconButton from "@material-ui/core/IconButton";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
+import IconButton from "@material-ui/core/IconButton";
 import Slide from "@material-ui/core/Slide";
-import "./styles.css";
-import CloseIcon from "@material-ui/icons/Close";
-import SearchIcon from "@material-ui/icons/Search";
-import MyLocationIcon from "@material-ui/icons/MyLocation";
 import CancelIcon from "@material-ui/icons/Cancel";
-import {
-  geoConvertAddressToLatLong,
-  geoConvertLatLongToAddress,
-} from "../../../utils/Geocode";
+import CloseIcon from "@material-ui/icons/Close";
+import MyLocationIcon from "@material-ui/icons/MyLocation";
+import SearchIcon from "@material-ui/icons/Search";
+import React, { useEffect, useState } from "react";
+import { geoConvertLatLongToAddress } from "../../../utils/Geocode";
+import "./styles.css";
+import Map from "./Map";
 
 const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="down" ref={ref} {...props} />;
@@ -25,11 +22,19 @@ const tileAlertLocation = "Vui lòng cấp quyền truy cập vị trí cho flas
 
 export default function DialogChangeAddress({ open, onClose }) {
   const [location, setLocation] = useState(null);
+  const [lat, setLat] = useState(0);
+  const [lng, setLng] = useState(0);
 
   const options = {
     enableHighAccuracy: true,
     timeout: 5000,
     maximumAge: 0,
+  };
+
+  const markCurrentLocationIntoGGMap = (pos) => {
+    const { latitude, longitude } = pos.coords;
+    setLat(latitude);
+    setLng(longitude);
   };
 
   const success = async (pos) => {
@@ -42,17 +47,18 @@ export default function DialogChangeAddress({ open, onClose }) {
   function errors(err) {
     console.warn(`ERROR(${err.code}): ${err.message}`);
   }
-  const hanleGetCurrentLocation = async () => {
+  const hanleGetCurrentLocation = async (callback) => {
     if (navigator.geolocation) {
       const result = await navigator.permissions.query({ name: "geolocation" });
 
       if (result.state === "granted") {
-        navigator.geolocation.getCurrentPosition(success);
+        navigator.geolocation.getCurrentPosition(callback);
       } else if (result.state === "prompt") {
-        navigator.geolocation.getCurrentPosition(success, errors, options);
+        navigator.geolocation.getCurrentPosition(callback, errors, options);
       } else if (result.state === "denied") {
         alert(tileAlertLocation);
       }
+
       result.onchange = function () {
         console.log(result.state);
       };
@@ -60,6 +66,10 @@ export default function DialogChangeAddress({ open, onClose }) {
       alert("Sorry Not available!");
     }
   };
+
+  useEffect(() => {
+    hanleGetCurrentLocation(markCurrentLocationIntoGGMap);
+  }, []);
 
   return (
     <Dialog
@@ -110,14 +120,13 @@ export default function DialogChangeAddress({ open, onClose }) {
             <MyLocationIcon
               fontSize="small"
               style={{ cursor: "pointer" }}
-              onClick={hanleGetCurrentLocation}
+              onClick={() => hanleGetCurrentLocation(success)}
             />
           )}
         </div>
+        <Map lat={lat} lng={lng} />
       </DialogContent>
-      {/* <div style={{ width: "100%" }}> */}
 
-      {/* </div> */}
       <DialogActions>
         {/* <Button onClick={() => onClose()} color="primary">
           Disagree
